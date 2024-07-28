47066d5393b1de554c16ea139ad2b3cf12564c48
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2024-07-28 23:03:43 +0900

    DenoスクリプトのCIを追加 (#2)

--- scripts/install_crates_io.ts
+++ scripts/install_crates_io.ts
@@ -1,6 +1,6 @@
-import { $ } from "https://deno.land/x/dax@0.39.1/mod.ts"
-import { confirm } from "npm:@inquirer/prompts@4.3.2"
-import pc from "npm:picocolors@1.0.0"
+import { $ } from "https://deno.land/x/dax@0.39.1/mod.ts";
+import { confirm } from "npm:@inquirer/prompts@4.3.2";
+import pc from "npm:picocolors@1.0.0";
 
 const errorText = `${pc.bold(pc.red("error"))}:`;
 
