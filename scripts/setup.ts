e33ee4b05caf2f7f6296b60ad9784889b2f5c89a
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2026-07-19 16:10:08 +0900

    システムパッケージの管理をmiseに移行

--- scripts/setup.ts
+++ scripts/setup.ts
@@ -1,36 +1,5 @@
 import { $ } from "https://deno.land/x/dax@0.39.1/mod.ts";
 import { confirm, input } from "npm:@inquirer/prompts@4.3.2";
-import pc from "npm:picocolors@1.0.0";
-
-const errorText = `${pc.bold(pc.red("error"))}:`;
-// const successText = `${pc.bold(pc.green("success"))}:`;
-
-// Check requirement commands
-const requirementCommands = [
-  "curl",
-  "git",
-  "unzip", // for Deno
-];
-for (const requirementCommand of requirementCommands) {
-  try {
-    await $`which ${requirementCommand}`.quiet();
-  } catch (_e) {
-    console.error(
-      errorText,
-      `${requirementCommand} command is not found. Please install it.`,
-    );
-    Deno.exit(1);
-  }
-}
-
-const isDirectoryExists = async (path: string): Promise<boolean> => {
-  try {
-    const fileInfo = await Deno.stat(path);
-    return fileInfo.isDirectory;
-  } catch (_e) {
-    return false;
-  }
-};
 
 const isAllYes = Deno.args.length > 0 &&
   (Deno.args[0] === "-y" || Deno.args[0] === "--yes");
@@ -53,10 +22,6 @@ const isBashSetUpConfirmed = isAllYes ||
 
 if (isBashSetUpConfirmed) {
   await $`ln -sf ${gitRootPath}/.bashrc ${homeDirectoryPath}/.bashrc`;
-  if (await isDirectoryExists("/usr/share/bash-completion")) {
-    await Deno.remove("/usr/share/bash-completion", { recursive: true });
-  }
-  await $`git clone --depth 1 https://github.com/scop/bash-completion.git /usr/share/bash-completion`;
 }
 
 // Nix
